37bfcc95b9cd1326d54cca8bddf0f633bab5953c
Y S Ramya <ysramya97@gmail.com>
2016-11-14 16:18:39 +0530

    Create Parser.java

---
+++ Parser.java
@@ -0,0 +1,23 @@
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+import org.xml.sax.helpers.*;
+import java.util.*;
+import java.io.*;
+
+public class Parser()
+{
+   private Hashtable tags;
+
+    public void startDocument() throws SAXException {
+        tags = new Hashtable();
+    }
+
+    public void endDocument() throws SAXException {
+        Enumeration e = tags.keys();
+        while (e.hasMoreElements()) {
+            String tag = (String)e.nextElement();
+            int count = ((Integer)tags.get(tag)).intValue();
+            System.out.println("Local Name \"" + tag + "\" occurs " 
+                               + count + " times");
+        }    
+}
