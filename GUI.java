d1c6c833248d01fe9a0895c7185ae984927ca902
Y S Ramya <ysramya97@gmail.com>
2016-11-14 16:11:16 +0530

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -5,5 +5,11 @@ public class GUI()
     JFrame main = new JFrame();
     JPanel output_panel=new JPanel();
     JPanel options=new Jpanel();
+    JPanel header=new JPanel();
+    JLabel title = new JLabel("DBLP Query Engine");
+    header.add(title);
+    main.add(header);
+    main.add(output_panel);
+    main.add(options);
   }
 }
